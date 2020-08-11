1ac9a4e9b6895b9a031eab23aa915f6c91fdde20
Alex Vance <avance@dotsln.com>
2020-08-10 20:57:12 -0500

    removed js files.

--- getFunctions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getFunctions(sls) {
-    return sls.service.getAllFunctions().map(function (fn) { return sls.service.getFunction(fn); });
-}
-exports.default = getFunctions;
-;
